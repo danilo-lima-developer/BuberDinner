1c64cb32fe211274ca0c312f939480542b0c508e
Danilo Lima <danilo.lima.developer@gmail.com>
2023-01-19 12:32:10 -0300

    Feat: Implementação do 'Jwt' e 'DataTime'

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services
         .AddAplication()
-        .AddInfrastructure();
+        .AddInfrastructure(builder.Configuration);
         
     builder.Services.AddControllers();
 }
